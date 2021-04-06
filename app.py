01c520378bdc683073d433fb596107680565779f
Elvira A.Kulikova <e.kulikova@norsi-trans.ru>
2021-04-06 14:28:27 +0300

    using chromedriver

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-import requests
+from selenium import webdriver
 
 from pages.quotes_page import  QuotePage
 
-page_content = requests.get('http://quotes.toscrape.com').content
-page = QuotePage(page_content)
+chrome = webdriver.Chrome(executable_path="c:/projects/chromedriver.exe")
+chrome.get('http://quotes.toscrape.com')
+#page_content = requests.get('http://quotes.toscrape.com').content
+page = QuotePage(chrome)
 
 for quote in page.quotes:
     print(quote.content)
